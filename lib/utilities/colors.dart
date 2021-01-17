d30d225bb3298989b17fad4c476691253ba1424c
m <shaimaa333salama@gmail.com>
2021-01-18 00:47:43 +0200

    grad_project

---
+++ lib/utilities/colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+const primary = Color(0xFF1a237e);
+const primaryDark = Color(0xFF000051);
+const primaryLight = Color(0xFF534bae);
+
+const secondary = Color(0xFF5c6bc0);
+const secondaryDark = Color(0xFF26418f);
+const secondaryLight = Color(0xFF8e99f3);
+
+const textOnPrimary = Color(0xFFffffff);
+const textOnSecondary = Color(0xFFffffff);
